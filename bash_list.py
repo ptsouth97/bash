339900e8c65c3e91d843fbf8dd7df65e3d0ebdfb
ptsouth97 <ptsouth97@gmail.com>
2017-02-13 22:37:56 -0500

    Added commands
    Put some more commands in bash_list.py

--- bash_list.py
+++ bash_list.py
@@ -6,5 +6,8 @@ commands=[
 ['cd ..', 'goes back one directory'],
 ['cd ../..', 'goes back 2 directories'],
 ['cd /', 'goes to the root directory'],
-['cd ~', 'goes to the home directory']
+['cd ~', 'goes to the home directory'],
+['mkdir', 'makes a new directory'],
+['rmdir', 'removes a directory if EMPTY'],
+['rm -r', 'removes a directory if NOT EMPTY']
 ]
